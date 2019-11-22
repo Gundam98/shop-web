2eee86145b407ad198cc4debe9200e2930dad151
Gundam98 <429300776@qq.com>
2019-11-23 00:18:54 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,200 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <el-container>
+      <el-header>
+        <el-row class="header">
+          <el-col :span="6">
+            <img
+              src="@/assets/logo.png"
+              class="logo"
+              style="width:60px;height:60px;float:left"
+            />
+          </el-col>
+          <el-col
+            :span="18"
+            style="display:flex;align-items:center;justify-content:flex-end;flex-direction:row;flex-wrap: wrap"
+          >
+            <el-menu
+              class="head-nav"
+              router
+              mode="horizontal"
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b"
+              v-if="login"
+            >
+              <el-menu-item index="/WantToBuyGoods">
+                我要买
+              </el-menu-item>
+              <el-menu-item index="/SellGoods">
+                我要卖
+              </el-menu-item>
+            </el-menu>
+            <el-dropdown
+              v-if="login"
+              style="float:right;display:flex;margin-left:10px"
+              @command="handleProfileCommand"
+            >
+              <el-avatar :src="circleUrl"></el-avatar>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="profile">账户信息</el-dropdown-item>
+                <el-dropdown-item command="Deposit">充值</el-dropdown-item>
+                <el-dropdown-item command="logout">注销</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+            <router-link
+              v-show="!login"
+              to="/Login"
+              style="text-decoration:none;color:white"
+            >
+              登陆/注册
+            </router-link>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-container class="main-content">
+        <el-aside>
+          <div class="side-bar">
+            <el-menu
+              router
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b"
+              style="height:100%"
+            >
+              <el-menu-item index="/">
+                主页
+              </el-menu-item>
+              <el-menu-item index="/WantToBuy">
+                求购
+              </el-menu-item>
+              <el-menu-item index="/AllGoods">
+                所有商品
+              </el-menu-item>
+            </el-menu>
+          </div>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+import api from "@/utils/api";
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      circleUrl:
+        "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+      login: false
+    };
+  },
+  created() {
+    if (localStorage.getItem("username")) {
+      this.login = true;
+    } else {
+      let _this = this;
+      api
+        .checkLogin()
+        .then(() => {
+          _this.login = true;
+        })
+        .catch(() => {
+          _this.login = false;
+        })
+        .then(() => {
+          _this.login = false;
+        });
+    }
+  },
+  updated() {
+    if (localStorage.getItem("username")) {
+      this.login = true;
+    } else {
+      let _this = this;
+      api
+        .checkLogin()
+        .then(() => {
+          _this.login = true;
+        })
+        .catch(() => {
+          _this.login = false;
+        })
+        .then(() => {
+          _this.login = false;
+        });
+    }
+  },
+  methods: {
+    handleProfileCommand(command) {
+      if (command === "profile") {
+        this.$router.push("/Profile");
+      } else if (command === "logout") {
+        if (api.logout()) {
+          localStorage.removeItem("username");
+          this.login = false;
+          this.$router.push("/");
+        } else {
+          this.$message.error("注销失败");
+        }
+      } else if (command === "Deposit") {
+        this.$router.push("/Deposit");
+      } else {
+        this.$message.error("系统出错啦");
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100vh;
+}
+
+body {
+  margin: 0;
+}
+
+.el-header {
+  line-height: 60px;
+  height: 60px;
+  margin: 0;
+  padding: 0;
+  background-color: #545c64;
+}
+
+.el-aside {
+  width: 200px !important;
+  line-height: 200px;
+}
+
+.head-nav {
+  float: right;
+}
+
+.side-bar {
+  height: 99%;
+}
+
+.main-content {
+  min-height: 91vh;
+}
+
+img {
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,119 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/WantToBuy",
+        name: "WantToBuy",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ("../views/WantToBuy.vue")
+    },
+    {
+        path: "/Profile",
+        name: "Profile",
+        meta: {
+            requireAuth: true
+        },
+        component: () =>
+            import ("../views/Profile.vue")
+    },
+    {
+        path: "/SellGoods",
+        name: "SellGoods",
+        meta: {
+            requireAuth: true
+        },
+        component: () =>
+            import ("../views/SellGoods.vue")
+    },
+    {
+        path: "/wantToBuyGoods",
+        name: "WantToBuyGoods",
+        meta: {
+            requireAuth: true
+        },
+        component: () =>
+            import ("../views/WantToBuyGoods.vue")
+    },
+    {
+        path: "/Login",
+        name: "Login",
+
+        component: () =>
+            import ("../views/Login.vue")
+    },
+    {
+        path: "/Register",
+        name: "Register",
+        component: () =>
+            import ("../views/Register.vue")
+    },
+    {
+        path: "/Deposit",
+        name: "Deposit",
+        meta: {
+            requireAuth: true
+        },
+        component: () =>
+            import ("../views/Deposit.vue")
+    },
+    {
+        path: "/Mine",
+        name: "Mine",
+        meta: {
+            requireAuth: true
+        },
+        component: () =>
+            import ("../views/Mine.vue")
+    },
+    {
+        path: "/AllGoods",
+        name: "AllGoods",
+        meta: {
+            requireAuth: true
+        },
+        component: () =>
+            import ("../views/AllGoods.vue")
+    },
+    {
+        path: "/Goods",
+        name: "Goods",
+        meta: {
+            requireAuth: true
+        },
+        component: () =>
+            import ("../views/Goods.vue")
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    base: process.env.BASE_URL,
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(res => res.meta.requireAuth)) {
+        // 判断是否需要登录权限
+        if (localStorage.getItem("username")) {
+            // 判断是否登录
+            next();
+        } else {
+            // 没登录则跳转到登录界面
+            next({
+                path: "/Login",
+                query: { redirect: to.fullPath }
+            });
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
