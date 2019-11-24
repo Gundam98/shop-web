99acc38c250484200073701c8a52a0bc87b16822
Gundam98 <429300776@qq.com>
2019-11-24 19:30:30 +0800

    完成竞价功能

--- src/views/Goods.vue
+++ src/views/Goods.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <el-page-header @back="goBack" content="详情"> </el-page-header>
     <el-row>
-      <el-col :span="12">
+      <el-col :span="10" style="padding:20px">
         <el-carousel>
           <el-carousel-item
             v-for="(url, index) in [goodsInfo.picUrl]"
@@ -16,10 +17,29 @@
         </el-carousel>
       </el-col>
       <el-col
-        :span="12"
+        :span="14"
         style="display:flex;flex-direction:column;flex-wrap: wrap;align-items:flex-start;justify-content:flex-start;padding:20px"
       >
-        <h1 style="font-size:30px">{{ goodsInfo.name }}</h1>
+        <div style="margin-bottom:20px;display:flex;align-items:center">
+          <el-tag
+            :type="tag.type"
+            effect="dark"
+            size="small"
+            style="margin-right:10px"
+          >
+            {{ tag.message }}
+          </el-tag>
+          <div style="font-size:40px">{{ goodsInfo.name }}</div>
+        </div>
+        <div style="margin-bottom:20px;">
+          <span>
+            距离结束还有<countdown
+              :endTime="goodsInfo.overTime"
+              style="color:red"
+            >
+            </countdown>
+          </span>
+        </div>
         <div>
           <span style="color:grey">当前价</span>
           <span style="color:#ff0036;font-size:30px">
@@ -31,66 +51,92 @@
           </span>
         </div>
         <br />
-        <el-form :inline="true" class="demo-form-inline">
+        <el-form :inline="true" v-if="goodsInfo.sellerUser !== curUser">
           <el-form-item>
-            <el-input v-model="bidPrice"></el-input>
+            <el-input v-model="bidPrice">
+              <template slot="prepend">￥</template>
+            </el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="bid">竞价</el-button>
+            <el-button type="primary" @click="bid"> 竞价 </el-button>
           </el-form-item>
         </el-form>
-        <div>
+
+        <div style="font-size:12px">
           <span style="margin-right:20px">
             <span style="color:grey">起拍价</span>
             ￥{{ goodsInfo.minPrice }}
           </span>
-          <span style="margin-left:20px">
+          <span style="margin-right:20px">
+            <span style="color:grey">卖家</span>
+            {{ goodsInfo.sellerUser }}
+          </span>
+          <span style="margin-right:20px">
             <span style="color:grey">上次修改时间</span>
             {{ goodsInfo.modifiedTime }}
           </span>
         </div>
       </el-col>
     </el-row>
+
+    <el-tabs type="card" value="description" style="padding:20px">
+      <el-tab-pane label="商品描述" name="description">
+        <p style="text-align: left">{{ goodsInfo.description }}</p>
+      </el-tab-pane>
+      <el-tab-pane label="服务说明" name="service">服务说明</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 import api from "@/utils/api";
 import formatTime from "@/utils/formatTime";
+import countdown from "@/components/Countdown";
 export default {
   data() {
     return {
       goodsInfo: {},
-      bidPrice: 0
+      bidPrice: null,
+      tag: {
+        type: "success",
+        message: "竞拍中"
+      },
+      curUser: ""
     };
+  },
+  components: {
+    countdown
   },
   created() {
     let _this = this;
+    this.curUser = localStorage.username;
     api.getGoodsInfo(this.$route.params.id).then(res => {
-      _this.goodsInfo = this.processGoodsInfo(res.data);
-      console.log(_this.goodsInfo);
+      //_this.goodsInfo = _this.processGoodsInfo(res.data);
+      _this.goodsInfo = _this.processGoodsInfo(res.data);
     });
   },
   methods: {
     processGoodsInfo: function(data) {
+      let _this = this;
       let info = data;
-      let i = 0;
       if (info.overTime < new Date().getTime()) {
         console.log("竞拍结束");
       } else {
         //时间戳转时间字符串
-        info.overTime = formatTime(info.overTime, "Y/M/D/ h:m:s");
         info.modifiedTime = formatTime(info.modifiedTime, "Y/M/D/ h:m:s");
+
+        //通过卖家id获取卖家名字
         api.getUserInfo(info.sellerUserId).then(res => {
-          //通过卖家id获取卖家名字
           info.sellerUser = res.data.username;
         });
+
+        //商品类型转换
         api.getGoodsType(info.type).then(res => {
-          //商品类型转换
           info.type = res.data.typeName;
         });
+
+        //通过竞价者id获取竞价者名字
         if (info.currentBuyerUserId !== null) {
-          //通过竞价者id获取竞价者名字
           let _this = this;
           api.getUserInfo(info.currentBuyerUserId).then(res => {
             _this.$set(_this.goodsInfo, "currentBuyer", res.data.username);
@@ -106,13 +152,36 @@ export default {
       return info;
     },
     bid() {
+      if (new Date(this.goodsInfo.overTime) < new Date().getTime()) {
+        this.$message.warning("竞拍已结束");
+        this.$router.push("/").catch(e => e);
+        return;
+      }
+      if (
+        !this.goodsInfo.currentBuyerPrice &&
+        this.goodsInfo.minPrice > this.bidPrice
+      ) {
+        this.$message.warning("出价至少高于最低价");
+        return;
+      }
+      if (
+        this.goodsInfo.currentBuyerPrice &&
+        this.goodsInfo.currentBuyerPrice > this.bidPrice
+      ) {
+        this.$message.warning("出价至少高于当前价");
+        return;
+      }
+
       let _this = this;
       api.bid(this.goodsInfo.id, this.bidPrice).then(res => {
         _this.goodsInfo.currentBuyerPrice = _this.bidPrice;
         _this.bidPrice = null;
-        _this.currentBuyerUser = localStorage.username;
-        _this.$message({ message: "竞价成功", type: "success" });
+        _this.goodsInfo.currentBuyer = localStorage.username;
+        _this.$message.success("竞价成功");
       });
+    },
+    goBack() {
+      this.$router.push("/").catch(e => e);
     }
   }
 };
