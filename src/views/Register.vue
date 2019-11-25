23c61f07ac327b83f2f2937aa00fc4c35054815f
Gundam98 <429300776@qq.com>
2019-11-25 20:31:10 +0800

    完成注册页面

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,94 @@
 <template>
   <div>
-    <h1>注册</h1>
+    <el-row>
+      <el-col
+        :span="12"
+        :offset="6"
+        label-position="right"
+        style="box-shadow:rgba(0, 0, 0, 0.12) 0px 2px 4px, rgba(0, 0, 0, 0.04) 0px 0px 6px; border-radius:5px; padding:20px; background-color:white"
+      >
+        <div style="font-size:30px;margin-bottom:10px">注册</div>
+        <el-form :model="registerForm" :ref="registerForm" status-icon>
+          <el-form-item prop="username">
+            <el-input
+              v-model="registerForm.username"
+              placeholder="用户名"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              v-model="registerForm.password"
+              placeholder="密码"
+              type="password"
+            >
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="realName">
+            <el-input
+              v-model="registerForm.realName"
+              placeholder="真实姓名"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="telephone">
+            <el-input
+              v-model="registerForm.telephone"
+              placeholder="联系电话"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="registerCity">
+            <el-input
+              v-model="registerForm.registerCity"
+              placeholder="注册城市"
+            ></el-input>
+          </el-form-item>
+          <el-form-item style="margin-top:20px;margin-bottom:0">
+            <el-row type="flex" justify="space-around">
+              <el-col :span="6">
+                <el-button type="primary" @click="register">注册</el-button>
+              </el-col>
+              <el-col :span="6">
+                <el-button type="primary" @click="resetForm">重置</el-button>
+              </el-col>
+            </el-row>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {};
+import api from "@/utils/api";
+export default {
+  data() {
+    return {
+      registerForm: {
+        username: "",
+        password: "",
+        realName: "",
+        telephone: "",
+        registerCity: ""
+      }
+    };
+  },
+  methods: {
+    resetForm: function() {
+      this.$refs["registerForm"].resetFields();
+    },
+    register: function() {
+      let _this = this;
+      api
+        .register(this.registerForm)
+        .then(res => {
+          _this.$message.success("注册成功");
+          _this.$router.push("/Login");
+        })
+        .catch(e => {
+          _this.$message.warnming("注册失败");
+        });
+    }
+  }
+};
 </script>
 
 <style></style>
