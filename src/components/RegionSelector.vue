dcccca98e133d7a84400be3abfabb1265aab3ef7
Gundam98 <429300776@qq.com>
2019-11-29 11:27:43 +0800

    添加地区选择组件

---
+++ src/components/RegionSelector.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-select v-model="value" placeholder="地区" filterable :size="size">
+    <el-option
+      v-for="province in provinces"
+      :key="province"
+      :label="province"
+      :value="province"
+    ></el-option>
+  </el-select>
+</template>
+
+<script>
+export default {
+  props: {
+    size: String,
+    defaultRegion: {
+      type: String,
+      default: ""
+    }
+  },
+  created() {
+    this.value = this.defaultRegion;
+  },
+  data() {
+    return {
+      provinces: [
+        "北京",
+        "浙江",
+        "天津",
+        "安徽",
+        "上海",
+        "福建",
+        "重庆",
+        "江西",
+        "山东",
+        "河南",
+        "内蒙古",
+        "湖北",
+        "新疆维吾尔",
+        "湖南",
+        "宁夏回族",
+        "广东",
+        "西藏",
+        "海南",
+        "广西壮族",
+        "四川",
+        "河北",
+        "贵州",
+        "山西",
+        "云南",
+        "辽宁",
+        "陕西",
+        "吉林",
+        "甘肃",
+        "黑龙江",
+        "青海",
+        "江苏"
+      ],
+      value: ""
+    };
+  },
+  watch: {
+    value: function() {
+      this.$emit("region", this.value);
+    }
+  }
+};
+</script>
+
+<style></style>
